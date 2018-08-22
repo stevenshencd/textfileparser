d9aae74d341486a8bdb75cc3416074f2f8040151
Shen <Steven.Shen@emc.com>
2018-08-22 16:24:55 +0800

    sort ITag/Stage list

--- textparser.py
+++ textparser.py
@@ -1,57 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-This is a general class to provide functions for text file analysis and report.
-"""
-import os
-import shutil
-from Tkinter import *
-import tkFileDialog
+# from __future__ import print_function
+#
+# """
+# This is a general class to provide functions for text file analysis and report.
+# """
 import csv
 import time
 
-class textparser:
 
+class TextParser(object):
     def __init__(self):
-        print "csv parsering started"
+        print("csv parsering started")
 
-    def openfile(self,filename):
-        if filename == '' :
-            filename = tkFileDialog.askopenfilename(initialdir='C:/')
-        return open(filename,"r")
+    @staticmethod
+    def opencsvfile(filename):
+        print("open CSV file")
+        return csv.reader(open(filename, "r"))
 
-    def opencsvfile(self,filename):
-        if filename == '' :
-            filename = tkFileDialog.askopenfilename(initialdir='C:/')
-        return csv.reader(open(filename,"r"))
-
-    def output(self,filename):
-        if filename == '' :
-            filename = tkFileDialog.asksaveasfilename()
+    @staticmethod
+    def output(filename):
         return open(filename, "a")
 
-    def inquery_pass(self):
-        csvdata = self.opencsvfile("")
-        totalpass = 0
-        for i, row in enumerate(csvdata):
-            # to add function for different parser and report, and to replace following lines
-            if i>0 :
-                totalpass = totalpass + int(row[1])
+    def sum_by_itag(self, inputpath):
+        itags = []  # list of execution status by I-Tag
+        record = {"ITag": "", "Stage": "", "Pass": 0, "Failed": 0, "NoRun": 0, "Blocked": 0, "NA": 0,
+                  "NotComplete": 0, "Others": 0}
 
-        fout = self.output("")
-        fout.write("total pass:" + str(totalpass))
-        fout.close()
-
-    def sumbyITag(self, inputpath):
-        ITags = [] # list of execution status by I-Tag
-        record = {"ITag":"","Stage":"","Pass":0,"Failed":0,"NoRun":0,"Blocked":0,"NA":0,"NotComplete":0,"Others":0}
         csvdata = self.opencsvfile(inputpath)
 
         for i, row in enumerate(csvdata):
             # to add function for different parser and report, and to replace following lines
-            if i > 0: #first row is head line, looping start from second row
-                if len(ITags) == 0: # for vacant ITags list
+            if i > 0:  # first row is head line, looping start from second row
+                if len(itags) == 0:  # for vacant itags list
                     record["ITag"] = row[1]
                     record["Stage"] = row[2]
                     status = row[3]
@@ -68,13 +50,13 @@ class textparser:
                     elif status == "N/A":
                         record["NA"] = 1
                     else:
-                        record["Others"] =1
-                    ITags.append(record)
+                        record["Others"] = 1
+                    itags.append(record)
                 else:
-                    ITagexisted = 0
-                    for r in ITags:
+                    itag_existed = 0
+                    for r in itags:
                         if r["ITag"] == row[1] and r["Stage"] == row[2]:
-                            ITagexisted = 1
+                            itag_existed = 1
                             status = row[3]
                             if status == "Passed":
                                 r["Pass"] = int(r["Pass"]) + 1
@@ -91,43 +73,46 @@ class textparser:
                             else:
                                 r["Others"] = int(r["Others"]) + 1
 
-                    if ITagexisted == 0:
-                        newItag = {"ITag":"","Stage":"","Pass":0,"Failed":0,"NoRun":0,"Blocked":0,"NA":0,"NotComplete":0,"Others":0}
-                        newItag["ITag"] = row[1]
-                        newItag["Stage"] = row[2]
+                    if itag_existed == 0:
+                        new_itag = {"ITag": row[1], "Stage": row[2], "Pass": 0, "Failed": 0, "NoRun": 0, "Blocked": 0,
+                                    "NA": 0, "NotComplete": 0, "Others": 0}
                         status = row[3]
                         if status == "Passed":
-                            newItag["Pass"] = 1
+                            new_itag["Pass"] = 1
                         elif status == "Failed":
-                            newItag["Failed"] = 1
+                            new_itag["Failed"] = 1
                         elif status == "No Run":
-                            newItag["NoRun"] = 1
+                            new_itag["NoRun"] = 1
                         elif status == "Blocked":
-                            newItag["Blocked"] = 1
+                            new_itag["Blocked"] = 1
                         elif status == "Not Completed":
-                            newItag["NotComplete"] = 1
+                            new_itag["NotComplete"] = 1
                         elif status == "N/A":
-                            newItag["NA"] = 1
+                            new_itag["NA"] = 1
                         else:
-                            newItag["Others"] = 1
+                            new_itag["Others"] = 1
 
-                        ITags.append(newItag)
+                        itags.append(new_itag)
+        # sort values by itag
+        itags.sort(key= lambda dic: dic["ITag"]+dic["Stage"])
         # write result to file
         reportdate = str(time.strftime("%Y-%m-%d", time.localtime()))
         fout = self.output("ITagreport_" + reportdate + ".csv")
         headline = "Date,ITag,Stage,Pass,Failed,Blocked,NoRun,NotComplete,NA,Others\n"
         fout.write(headline)
-        for item in ITags:
-            line = reportdate + "," + item["ITag"] + "," +item["Stage"] + "," + str(item["Pass"]) + "," + str(item["Failed"]) + "," \
-                   + str(item["Blocked"]) + "," + str(item["NoRun"]) + "," + str(item["NotComplete"]) \
-                   + "," + str(item["NA"]) + "," + str(item["Others"]) + "\n"
+        for item in itags:
+            line = reportdate + "," + item["ITag"] + "," + item["Stage"] + "," + str(item["Pass"]) + "," + str(
+                item["Failed"]) + "," +\
+                str(item["Blocked"]) + "," + str(item["NoRun"]) + "," + str(item["NotComplete"]) +\
+                "," + str(item["NA"]) + "," + str(item["Others"]) + "\n"
             fout.write(line)
         fout.close()
-        print "summing by ITag is successful"
+        print("summing by ITag is successful")
 
-    def sumbyStage(self, inputpath):
+    def sum_by_stage(self, inputpath):
         stages = []  # list of execution status by I-Tag
-        record = {"Stage": "", "Pass": 0, "Failed": 0, "NoRun": 0, "Blocked": 0, "NA": 0, "NotComplete": 0, "Others": 0}
+        record = {"Stage": "", "Pass": 0, "Failed": 0, "NoRun": 0, "Blocked": 0, "NA": 0, "NotComplete": 0,
+                  "Others": 0}
         csvdata = self.opencsvfile(inputpath)
         for i, row in enumerate(csvdata):
             # to add function for different parser and report, and to replace following lines
@@ -173,42 +158,43 @@ class textparser:
                                 r["Others"] = int(r["Others"]) + 1
 
                     if stageexisted == 0:
-                        newStage = {"Stage": "", "Pass": 0, "Failed": 0, "NoRun": 0, "Blocked": 0, "NA": 0, "NotComplete": 0,
-                                   "Others": 0}
-                        newStage["Stage"] = row[2]
+                        new_stage = {"Stage": row[2], "Pass": 0, "Failed": 0, "NoRun": 0, "Blocked": 0, "NA": 0,
+                                     "NotComplete": 0, "Others": 0}
                         status = row[3]
                         if status == "Passed":
-                            newStage["Pass"] = 1
+                            new_stage["Pass"] = 1
                         elif status == "Failed":
-                            newStage["Failed"] = 1
+                            new_stage["Failed"] = 1
                         elif status == "No Run":
-                            newStage["NoRun"] = 1
+                            new_stage["NoRun"] = 1
                         elif status == "Blocked":
-                            newStage["Blocked"] = 1
+                            new_stage["Blocked"] = 1
                         elif status == "Not Completed":
-                            newStage["NotComplete"] = 1
+                            new_stage["NotComplete"] = 1
                         elif status == "N/A":
-                            newStage["NA"] = 1
+                            new_stage["NA"] = 1
                         else:
-                            newStage["Others"] = 1
+                            new_stage["Others"] = 1
 
-                        stages.append(newStage)
+                        stages.append(new_stage)
+        # sort values by stage
+        stages.sort(key=lambda dic: dic["Stage"])
         # write result to file
         reportdate = str(time.strftime("%Y-%m-%d", time.localtime()))
-        fout = self.output("Stagereport_"+ reportdate + ".csv")
+        fout = self.output("Stagereport_" + reportdate + ".csv")
 
         headline = "Date,Stage,Pass,Failed,Blocked,NoRun,NotComplete,NA,Others\n"
         fout.write(headline)
         for item in stages:
-            line = reportdate + "," + item["Stage"] + "," + str(item["Pass"]) + "," + str(item["Failed"]) + "," \
-                   + str(item["Blocked"]) + "," + str(item["NoRun"]) + "," + str(item["NotComplete"]) \
-                   + "," + str(item["NA"]) + "," + str(item["Others"]) + "\n"
+            line = reportdate + "," + item["Stage"] + "," + str(item["Pass"]) + "," + str(item["Failed"]) + "," +\
+                str(item["Blocked"]) + "," + str(item["NoRun"]) + "," + str(item["NotComplete"]) +\
+                "," + str(item["NA"]) + "," + str(item["Others"]) + "\n"
             fout.write(line)
         fout.close()
-        print "summing by stage is successful"
+        print("summing  by stage is successful")
 
 if __name__ == '__main__':
     inputpath = "utms_report_20180730-020541.csv"
-    textparser().sumbyITag(inputpath)
-    textparser().sumbyStage(inputpath)
+    TextParser().sum_by_itag(inputpath)
+    TextParser().sum_by_stage(inputpath)
 
